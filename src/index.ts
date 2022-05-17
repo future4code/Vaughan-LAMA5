a565d8aa9f73433273539871c5e07336c355c9da
MaiaJrDev <rm.dcjr@hotmail.com>
2022-05-17 11:37:33 -0300

    att package

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { AddressInfo } from "net";
 import express from "express";
 import { userRouter } from "./routes/userRouter";
 import { bandRouter } from "./routes/bandRouter";
+import cors from 'cors';
+
 dotenv.config();
 const app = express();
 
-app.use(express.json() as express.RequestHandler);
+app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/band", bandRouter);
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
